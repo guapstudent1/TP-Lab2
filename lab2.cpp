e6fb172ad54ac421c033bb62337569f9c0438f40
Alexander Ryzhkov <al.pijcov@gmail.com>
2018-09-15 15:47:39 +0300

    Некоторые изменения, для просмотра возможностей Гита в Винде

--- lab2.cpp
+++ lab2.cpp
@@ -2,11 +2,15 @@
 #include <string>
 #include <fstream>
 
+using namespace std;
+
 int main()
 {
 	// С использованием файловых и строковых потоков написать программу,
 	// которая считывает текст из файла и выводит его на экран, меняя местами
 	// каждые два соседних слова
-
+	char c;
+	cout << "Hello user" << endl;
+	cin >> c;
 	return 0;
 }
